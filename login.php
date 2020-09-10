7d07dafeed2dfe7ef70ddfa3c24130572627f677
cesart050892 <cesart050892@gmail.com>
2020-09-10 14:08:52 -0600

    first

--- login.php
+++ login.php
@@ -1,24 +1,38 @@
 <?php
-
+  
+  //Inicio de la sesión
   session_start();
-
+  //Verficamos si la sesión tiene la variable requerida
   if (isset($_SESSION['user_id'])) {
-    header('Location: /php-login');
+    //Redirecionamos index.php
+    header('Location: /php-login-simple');
   }
+  //Requerimos la base de datos
+  //para la comprobación de las credenciales
   require 'database.php';
 
+  //Verificamos las variables POST
   if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    //Hacemos la preparación de la consulta con la clausula email
     $records = $conn->prepare('SELECT id, email, password FROM users WHERE email = :email');
+    //Emparejamos la variable protegida con POST[email]
     $records->bindParam(':email', $_POST['email']);
+    //Ejecutamos la consulta
     $records->execute();
+    //Guardamos el unico resultado fetch a result
     $results = $records->fetch(PDO::FETCH_ASSOC);
 
+    //Iniciamos la variable message
     $message = '';
-
+    //Verificamos que el resultado sea mayor que 1
+    //y comparamos la variable POST con la respuesta de la base de datos
     if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+      //Creamos la variable de sesion y le asignamos el id
       $_SESSION['user_id'] = $results['id'];
-      header("Location: /php-login");
+      //redireciona a index.php
+      header("Location: /php-login-simple");
     } else {
+       //Mensaje de error de comprobacion de credenciales
       $message = 'Sorry, those credentials do not match';
     }
   }
@@ -34,6 +48,7 @@
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
+    <!-- Header -->
     <?php require 'partials/header.php' ?>
 
     <?php if(!empty($message)): ?>
@@ -44,8 +59,8 @@
     <span>or <a href="signup.php">SignUp</a></span>
 
     <form action="login.php" method="POST">
-      <input name="email" type="text" placeholder="Enter your email">
-      <input name="password" type="password" placeholder="Enter your Password">
+      <input name="email" type="text" placeholder="Enter your email" autocomplete="off">
+      <input name="password" type="password" placeholder="Enter your Password" autocomplete="off">
       <input type="submit" value="Submit">
     </form>
   </body>
